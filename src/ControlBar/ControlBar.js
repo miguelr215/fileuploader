b927bbc5ffe586d578aebf06d1f83d5d4176fe89
drexelramos <drexelramos@gmail.com>
2020-10-15 05:47:41 -0700

    added components

---
+++ src/ControlBar/ControlBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ControlBar extends React.Component {
+    render(){
+        return(
+            <div>control bar</div>
+        );
+    }
+}
+
+export default ControlBar;
---
+++ src/FilterableList/FilterableList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './FilterableList.css';
+import ListItem from '../ListItem/ListItem.js';
+
+class FilterableList extends React.Component {
+    render(){
+        const list = this.props.file
+            .map((file, key) => <ListItem {...file} key={key}/>);
+        return(
+            <div className='FilterableList'>{list}</div>
+        );
+    }
+}
+
+FilterableList.defaultProps = {
+    files: []
+};
+
+export default FilterableList;
